6dd5299f20b28a8b3064698bbd0d2127180278a7
Yosuke Ota <otameshiyo23@gmail.com>
2025-02-01 10:58:11 +0900

    feat: Improved babel-plugin to optimize output (#87)
    * feat: Improved babel-plugin to optimize output
    
    * Create stale-wombats-ring.md

--- packages/babel-plugin/tests/src/fixtures/test/simple-output.mjs
+++ packages/babel-plugin/tests/src/fixtures/test/simple-output.mjs
@@ -1,2 +1,2 @@
-import { BigNum as _BigNum, toResult as _toResult } from "@bignum/template/core";
-console.log(_toResult(_BigNum.valueOf(1).add(2).add(3)));
+import { add as _add, toResult as _toResult } from "@bignum/template/core";
+console.log(_toResult(_add(_add(1, 2), 3)));
