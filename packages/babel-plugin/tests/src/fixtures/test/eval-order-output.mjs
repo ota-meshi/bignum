5009df97c774d27831b31f043d6180237b87159f
Yosuke Ota <otameshiyo23@gmail.com>
2025-02-03 09:45:48 +0900

    feat: Improved babel-plugin to optimize output (#95)
    * feat: Improved babel-plugin to optimize output
    
    * Create mean-pugs-crash.md

--- packages/babel-plugin/tests/src/fixtures/test/eval-order-output.mjs
+++ packages/babel-plugin/tests/src/fixtures/test/eval-order-output.mjs
@@ -1,5 +1,4 @@
 import { add as _add, multiply as _multiply, toResult as _toResult } from "@bignum/template/core";
-import * as t from "@bignum/template";
 export default function test() {
   let i = 3;
   return [_toResult(_multiply(i++, _add(i++, i++)))];
